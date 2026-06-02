12fc02c6ae2920d0006f70036c396a1f23b59597
Krzysztof Węcławik <krzysztof.weclawik@gmail.com>
2026-06-03 00:55:29 +0200

    feat: add get_pending_links and update_link queries

--- db/queries/link.py
+++ db/queries/link.py
@@ -20,3 +20,38 @@ def save_links(config_hash, links_dict: dict):
     
     finally:
         conn.close()
+
+def get_pending_links(config_hash):
+    conn = get_connection()
+    try:
+        cur = conn.cursor()
+        cur.execute("""
+            SELECT id, url FROM link
+            JOIN config ON config.id = config_id
+            JOIN status ON status.id = status_id
+            WHERE config.hash = %s
+            AND status.name = 'pending'
+        """, (config_hash, ))
+
+        return cur.fetchall()
+    
+    finally:
+        conn.close()
+
+def update_link(link_id, content, embedding, status):
+    conn = get_connection()
+    try:
+        cur = conn.cursor()
+        cur.execute("""
+            UPDATE link
+            SET content = %s,
+                embedding = %s,
+                status_id = (SELECT id FROM status WHERE name = %s)
+            WHERE id = %s
+        """, (content, embedding, status, link_id))
+        conn.commit()
+        
+        return True
+    
+    finally:
+        conn.close()
