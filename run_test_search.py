7ee8b76782d88781b59b172cd763f2c7e8460296
Krzysztof Węcławik <krzysztof.weclawik@gmail.com>
2026-06-04 08:46:44 +0200

    feat: add test search script

---
+++ run_test_search.py
@@ -0,0 +1,4 @@
+from test_search import test_search
+
+if __name__ == "__main__":
+    test_search()
---
+++ test_search.py
@@ -0,0 +1,16 @@
+from embeddings import get_embedding
+from db.queries.link import search_links, get_links
+
+def test_search():
+    while True:
+        query = input("Zapytanie: ")
+        if not query:
+            continue
+        
+        embedding = get_embedding("query: " + query)
+        link_ids = search_links(embedding, limit=100)
+        links = get_links(link_ids)
+        
+        for i, link in enumerate(links, 1):
+            print(f"{i}. {link['title']}")
+        print("---")
