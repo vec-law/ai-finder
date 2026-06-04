5044835a02264bf94ba420450f57f971c0c5d58f
Krzysztof Węcławik <krzysztof.weclawik@gmail.com>
2026-06-04 09:03:18 +0200

    feat: add fetcher deduplication in save_fetcher

--- db/queries/fetcher.py
+++ db/queries/fetcher.py
@@ -5,6 +5,16 @@ def save_fetcher(config_hash, url):
     try:
         cur = conn.cursor()
         cur.execute("""
+            SELECT id FROM fetcher
+            WHERE config_id = (SELECT id FROM config WHERE hash = %s)
+            AND url = %s
+        """, (config_hash, url))
+
+        result = cur.fetchone()
+        if result:
+            return result[0]
+
+        cur.execute("""
             INSERT INTO fetcher (config_id, url)
             VALUES (
                 (SELECT id FROM config WHERE hash = %s),
@@ -15,7 +25,6 @@ def save_fetcher(config_hash, url):
         conn.commit()
 
         result = cur.fetchone()
-
         if not result:
             return None
         
